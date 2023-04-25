91cfdfc4b33df14c9378d7d585e45a852f44f4ef
Maks Zachary Walewski <maks.walewski@gmail.com>
2023-04-25 16:25:01 +0200

    prepare_so_coupling.py: made a function (to be imported elsewhere) & command line script

--- molscat_data/prepare_so_coupling.py
+++ molscat_data/prepare_so_coupling.py
@@ -3,19 +3,66 @@ from pathlib import Path
 
 from matplotlib import pyplot as plt
 
+import argparse
+
 from _molscat_data.physical_constants import hartree_in_inv_cm, fine_structure_constant
 
-so_coupling_path = Path(__file__).parents[1] / 'data' / 'so_coupling' / 'lambda_SO_a_SrRb+_MT_original.dat'
-so_coupling = np.loadtxt(so_coupling_path, dtype = float)
-print(1/(hartree_in_inv_cm**2 * fine_structure_constant**2))
-so_coupling[:,1] /= (hartree_in_inv_cm**2 * fine_structure_constant**2)
-so_coupling[:,1] -=  1/(so_coupling[:,0]**3*hartree_in_inv_cm)
-print(1/hartree_in_inv_cm)
+def scale_so_and_write(input_path, output_path, scaling, header = None):
+    
+    Path(output_path).parent.mkdir(parents = True, exist_ok = True)
 
-plt.scatter(so_coupling[:,0], so_coupling[:,1])
-plt.show()
+    so_coupling = np.loadtxt(input_path, dtype = float)
+    
+    C3 = 1/hartree_in_inv_cm
+    number_of_points = so_coupling.shape[0]
+    
+    if header == None:
+        header = (  f'# first line anyway ignored (n. of points, energy shift, scaling = scaling x scaling, unit of disctance, unit of energy [both in units defined in POTL block])\n'
+                    f'{number_of_points} 0.0 1.0 1.0 1.0\n'
+                    f'# also ignored (3 integer parameters n, m, and s controling the RKHS, if we want to impose conditions, number of coefficients we want to impose conditions on)\n'
+                    f'3 0 3 T 1\n'
+                    f'# also ignored (Ra, C3, RC3)\n'
+                    f'25.0 {C3:.15e} 50.0\n'
+                    f'# also ignored'
+                    )
+    
+
+    so_coupling[:,1] /= (hartree_in_inv_cm**2 * fine_structure_constant**2)
+    so_coupling[:,1] *= scaling
+    so_coupling[:,1] -=  C3/so_coupling[:,0]**3
+    np.savetxt(output_path, so_coupling, fmt = ['%.2f','%.15e'], header = header, comments = '')
+
+
+def main():
+    parser_description = f"""This script prepares the input for RKHS routine in molscat
+    for the given second-order spin-orbit coupling in the short range
+    and the scaling of its short-range part. The pure spin-spin interaction
+    term is added."""
+    parser = argparse.ArgumentParser(description=parser_description)
+    parser.add_argument("-i", "--input", type = str, required = True, help = "Path to the input file or directory")
+    parser.add_argument("-o", "--output", type = str, required = True, help = "Path to the output file or directory")
+    parser.add_argument("-s", "--scaling", type = float, default = 1., help = "Scaling of the short-range part of the potential.")
+    args = parser.parse_args()
+
+    scale_so_and_write(args.input, args.output, args.scaling)
+
+if __name__ == '__main__':
+    main()
+
+    # new_so_coupling_path = so_coupling_path.with_stem('1st_pt_lambda_SO+SS_MT_Hartrees')
+
+
+# so_coupling_path = Path(__file__).parents[1] / 'data' / 'so_coupling' / 'lambda_SO_a_SrRb+_MT_original.dat'
+# so_coupling = np.loadtxt(so_coupling_path, dtype = float)
+# print(1/(hartree_in_inv_cm**2 * fine_structure_constant**2))
+# so_coupling[:,1] /= (hartree_in_inv_cm**2 * fine_structure_constant**2)
+# so_coupling[:,1] -=  1/(so_coupling[:,0]**3*hartree_in_inv_cm)
+# print(1/hartree_in_inv_cm)
+
+# plt.scatter(so_coupling[:,0], so_coupling[:,1])
+# plt.show()
 
 
 
-new_so_coupling_path = so_coupling_path.with_stem('1st_pt_lambda_SO+SS_MT_Hartrees')
-np.savetxt(new_so_coupling_path, so_coupling, fmt = ['%.2f','%.15e'])
+# new_so_coupling_path = so_coupling_path.with_stem('1st_pt_lambda_SO+SS_MT_Hartrees')
+# np.savetxt(new_so_coupling_path, so_coupling, fmt = ['%.2f','%.15e'])
