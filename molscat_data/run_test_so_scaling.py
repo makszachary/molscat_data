69d23111b80ed4e036e95d2079675bc5150bc652
Maks Zachary Walewski <maks.walewski@gmail.com>
2023-04-26 20:35:37 +0200

    run_test_so_scaling.py: nenergies changed back to 100

--- molscat_data/run_test_so_scaling.py
+++ molscat_data/run_test_so_scaling.py
@@ -29,7 +29,7 @@ from prepare_so_coupling import scale_so_and_write
 singlet_scaling_path = Path(__file__).parents[1].joinpath('data', 'scaling_old', 'singlet_vs_coeff.json')
 triplet_scaling_path = Path(__file__).parents[1].joinpath('data', 'scaling_old', 'triplet_vs_coeff.json')
 
-E_min, E_max, nenergies, n = 4e-7, 4e-3, 5, 3
+E_min, E_max, nenergies, n = 4e-7, 4e-3, 100, 3
 energy_tuple = tuple( round(n_root_scale(i, E_min, E_max, nenergies-1, n = n), sigfigs = 11) for i in range(nenergies) )
 molscat_energy_array_str = str(energy_tuple).strip(')').strip('(')
 scratch_path = Path(os.path.expandvars('$SCRATCH'))
