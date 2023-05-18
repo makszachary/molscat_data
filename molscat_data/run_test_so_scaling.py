75adcf2e59e0880a9728d54dfca32d6fcdfb7ebf
Maks Zachary Walewski <maks.walewski@gmail.com>
2023-05-18 17:38:59 +0200

    changes for compatibility between scripts

--- molscat_data/run_test_so_scaling.py
+++ molscat_data/run_test_so_scaling.py
@@ -209,7 +209,7 @@ def main():
     ### COLLECT S-MATRIX AND PICKLE IT ####
     # output_dir = Path(__file__).parents[1].joinpath('molscat', 'outputs', 'RbSr+_tcpld', f'{nenergies}_E', f'{args.singlet_phase}_{args.triplet_phase}')
     # pickle_paths = []
-    # output_dirs = tuple( pickles_dir_path / 'RbSr+_tcpld_so_scaling' / f'{nenergies}_E' / f'{phases[0][0]:.4f}_{phases[0][1]:.4f}' / f'{so_scaling:.4f}' for so_scaling in so_scaling_values )
+    # output_dirs = tuple( scratch_path / 'molscat' / 'outputs' / 'RbSr+_tcpld_so_scaling' / f'{nenergies}_E' / f'{phases[0][0]:.4f}_{phases[0][1]:.4f}' / f'{so_scaling:.4f}.pickle' for so_scaling in so_scaling_values )
     # for output_dir, so_scaling in zip(output_dirs, so_scaling_values):
     #     s_matrix_collection, duration, output_dir, pickle_path = collect_and_pickle( output_dir, so_scaling )
     #     pickle_paths.append(pickle_path)
@@ -218,7 +218,7 @@ def main():
     ### LOAD S-MATRIX, CALCULATE THE EFFECTIVE PROBABILITIES AND WRITE THEM TO .TXT FILE ###
     # pickle_path = Path(__file__).parents[1].joinpath('data_produced', 'pickles', 'RbSr+_tcpld_100_E.pickle')
     # pickle_path = Path(__file__).parents[1].joinpath('data_produced', 'pickles', 'RbSr+_tcpld', '10_E', f'{args.singlet_phase}_{args.triplet_phase}.pickle')
-    pickle_paths = tuple( scratch_path / 'molscat' / 'outputs' / 'RbSr+_tcpld_so_scaling' / f'{nenergies}_E' / f'{phases[0][0]:.4f}_{phases[0][1]:.4f}' / f'{so_scaling:.4f}.pickle' for so_scaling in so_scaling_values )
+    pickle_paths = tuple( pickles_dir_path / 'RbSr+_tcpld_so_scaling' / f'{nenergies}_E' / f'{phases[0][0]:.4f}_{phases[0][1]:.4f}' / f'{so_scaling:.4f}' for so_scaling in so_scaling_values )
     # for pickle_path in pickle_paths:
     #     calculate_and_save_the_peff_parallel(pickle_path, phases[0], dLMax = args.dLMax)
 
