3ae37e56311297239aff807e0c950d2994fe92b4
Maks Zachary Walewski <maks.walewski@gmail.com>
2023-08-09 14:55:49 +0200

    utils.py: test changes for optimization of calculating probabilities

--- molscat_data/_molscat_data/utils.py
+++ molscat_data/_molscat_data/utils.py
@@ -26,6 +26,7 @@ from _molscat_data.physical_constants import amu_to_au
 def rate_fmfsms(s_matrix_collection: SMatrixCollection, F_out: int, MF_out: int, S_out: int, MS_out: int, F_in: int, MF_in: int, S_in: int, MS_in: int, param_indices: dict = None, dLMax: int = 4, unit = None) -> float:
     L_max = max(key[0].L for s_matrix in s_matrix_collection.matrixCollection.values() for key in s_matrix.matrix.keys())
     rate = np.sum( [ s_matrix_collection.getRateCoefficient(qn.LF1F2(L = L_out, ML = ML_in + MF_in + MS_in - MF_out - MS_out, F1 = F_out, MF1 = MF_out, F2 = S_out, MF2 = MS_out), qn.LF1F2(L = L_in, ML = ML_in, F1 = F_in, MF1 = MF_in, F2 = S_out, MF2 = MS_in), unit = unit, param_indices = param_indices) for L_in in range(0, L_max+1, 2) for ML_in in range(-L_in, L_in+1, 2) for L_out in range(L_in - dLMax*2, L_in + dLMax*2+1, 2*2) if (L_out >= 0 and L_out <=L_max) ], axis = 0 )
+    print(rate)
     return rate
 
 def rate_fmfsms_vs_L(s_matrix_collection: SMatrixCollection, F_out: int, MF_out: int, S_out: int, MS_out: int, F_in: int, MF_in: int, S_in: int, MS_in: int, param_indices: dict = None, dLMax: int = 4, unit = None) -> float:
@@ -125,8 +126,6 @@ def probability_not_parallel(s_matrix_collection: SMatrixCollection, F_out: int 
 
     if array_like:
         arguments = tuple( (s_matrix_collection, *(args[name][index] for name in args), param_indices, dLMax) for index in np.ndindex(arg_shapes[0]))
-        print(arguments)
-        [print(*arg) for arg in arguments]
         results = [ rate_fmfsms(*arg) for arg in arguments ]
         rate_shape = results[0].shape
         rate = np.array(results).reshape((*arg_shapes[0], *rate_shape))
