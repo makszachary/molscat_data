b20ca5b0c4fb4459398101f2d23cef03dade6cea
Maks Zachary Walewski <maks.walewski@gmail.com>
2023-08-18 20:28:29 +0200

    run_88Sr_vs_DPhi.py: added the possibility of writing k_L_E

--- molscat_data/_molscat_data/utils.py
+++ molscat_data/_molscat_data/utils.py
@@ -32,7 +32,7 @@ def rate_fmfsms(s_matrix_collection: SMatrixCollection, F_out: int, MF_out: int,
 
 def rate_fmfsms_vs_L(s_matrix_collection: SMatrixCollection, F_out: int, MF_out: int, S_out: int, MS_out: int, F_in: int, MF_in: int, S_in: int, MS_in: int, param_indices: dict = None, dLMax: int = 4, unit = None) -> float:
     L_max = max(key[0].L for s_matrix in s_matrix_collection.matrixCollection.values() for key in s_matrix.matrix.keys())
-    rate = np.array( [np.sum([ s_matrix_collection.getRateCoefficient(qn.LF1F2(L = L_out, ML = ML_in + MF_in + MS_in - MF_out - MS_out, F1 = F_out, MF1 = MF_out, F2 = S_out, MF2 = MS_out), qn.LF1F2(L = L_in, ML = ML_in, F1 = F_in, MF1 = MF_in, F2 = S_out, MF2 = MS_in), unit = unit, param_indices = param_indices) for ML_in in range(-L_in, L_in+1, 2) for L_out in range(L_in - dLMax*2, L_in + dLMax*2+1, 2*2) if (L_out >= 0 and L_out <=L_max) ]) for L_in in range(0, L_max+1, 2)])
+    rate = np.array( [np.sum([ s_matrix_collection.getRateCoefficient(qn.LF1F2(L = L_out, ML = ML_in + MF_in + MS_in - MF_out - MS_out, F1 = F_out, MF1 = MF_out, F2 = S_out, MF2 = MS_out), qn.LF1F2(L = L_in, ML = ML_in, F1 = F_in, MF1 = MF_in, F2 = S_out, MF2 = MS_in), unit = unit, param_indices = param_indices) for ML_in in range(-L_in, L_in+1, 2) for L_out in range(L_in - dLMax*2, L_in + dLMax*2+1, 2*2) if (L_out >= 0 and L_out <=L_max) ], axis = 0) for L_in in range(0, L_max+1, 2)])
     return rate
 
 def rate_Lfmfsms(s_matrix_collection: SMatrixCollection, L_in: int, F_out: int, MF_out: int, S_out: int, MS_out: int, F_in: int, MF_in: int, S_in: int, MS_in: int, param_indices: dict = None, param_values: dict = None, dLMax: int = 4, unit = None) -> float:
@@ -175,7 +175,7 @@ def k_L_E_not_parallel(s_matrix_collection: SMatrixCollection, F_out: int | np.n
         results = [ rate_fmfsms_vs_L(*arg) for arg in arguments ]
         rate_shape = results[0].shape
         rate = np.array(results).reshape((*arg_shapes[0], *rate_shape))
-        momentum_transfer_rate = np.full_like(rate, momentum_transfer_rate)
+        momentum_transfer_rate = np.full_like(rate, momentum_transfer_rate.squueze())
 
         return rate, momentum_transfer_rate
     
