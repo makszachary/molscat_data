25d349bf1be2d808412d85325f8d42fb3b8b5eb0
Maks Zachary Walewski <maks.walewski@gmail.com>
2023-08-18 21:19:02 +0200

    run_88Sr_vs_DPhi.py: test

--- molscat_data/run_88Sr_vs_DPhi.py
+++ molscat_data/run_88Sr_vs_DPhi.py
@@ -289,7 +289,7 @@ def calculate_and_save_k_L_E_and_peff_not_parallel(pickle_path: Path | str, phas
     F_out, F_in, S = 2, 2, 1
     MF_out, MS_out, MF_in, MS_in = np.meshgrid(np.arange(-F_out, F_out+1, 2), -S, np.arange(-F_in, F_in+1, 2), S, indexing = 'ij')
     # only for test:
-    MF_out, MS_out, MF_in, MS_in = np.meshgrid(np.arange(-F_out, -F_out+3, 2), -S, np.arange(-F_in, -F_in+1, 2), S, indexing = 'ij')
+    # MF_out, MS_out, MF_in, MS_in = np.meshgrid(np.arange(-F_out, -F_out+3, 2), -S, np.arange(-F_in, -F_in+1, 2), S, indexing = 'ij')
     arg_cold_spin_change_lower = (s_matrix_collection, F_out, MF_out, S, MS_out, F_in, MF_in, S, MS_in, param_indices, dLMax)
 
     args = [arg_hpf_deexcitation, arg_cold_spin_change_higher, arg_cold_spin_change_lower]
