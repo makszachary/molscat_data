6462f00f4e165d6cd23ddafd5be13e632f680506
Maks Zachary Walewski <maks.walewski@gmail.com>
2023-08-21 17:17:25 +0200

    run_88Sr_vs_DPhi_SE.py: small fix

--- molscat_data/run_88Sr_vs_DPhi_SE.py
+++ molscat_data/run_88Sr_vs_DPhi_SE.py
@@ -397,8 +397,8 @@ def main():
         t0 = time.perf_counter()
         pickle_paths = tuple( pickles_dir_path / args.input_dir_name / f'{E_min:.2e}_{E_max:.2e}_{nenergies}_E' / f'{singlet_phase:.4f}_{triplet_phase:.4f}.pickle' for singlet_phase, triplet_phase in phases)
         arguments = tuple( (pickle_path, phase, temperatures) for pickle_path, phase in zip(pickle_paths, phases) )
-        # pool.starmap(calculate_and_save_k_L_E_SE_and_peff_not_parallel, arguments)
-        [calculate_and_save_k_L_E_SE_and_peff_not_parallel(*arg) for arg in arguments]
+        pool.starmap(calculate_and_save_k_L_E_SE_and_peff_not_parallel, arguments)
+        # [calculate_and_save_k_L_E_SE_and_peff_not_parallel(*arg) for arg in arguments]
         print(f'The time of calculating all the probabilities for all singlet, triplet phases was {time.perf_counter()-t0:.2f} s.')
     
     # [plot_probability_vs_DPhi(singlet_phase, triplet_phases = triplet_phases, energy_tuple = energy_tuple, temperatures = temperatures, input_dir_name = args.input_dir_name, plot_temperature=temperature) for temperature in temperatures]
