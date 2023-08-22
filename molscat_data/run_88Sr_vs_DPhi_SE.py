249dac26ad936c4f421863d064e5b9a389fbae6e
Maks Zachary Walewski <maks.walewski@gmail.com>
2023-08-22 11:32:20 +0200

    run_88Sr_vs_DPhi_SE.py: test

--- molscat_data/run_88Sr_vs_DPhi_SE.py
+++ molscat_data/run_88Sr_vs_DPhi_SE.py
@@ -181,9 +181,9 @@ def calculate_and_save_k_L_E_SE_and_peff_not_parallel(pickle_path: Path | str, p
 
         distribution_arrays = [np.fromiter(n_root_iterator(temperature = temperature, E_min = min(s_matrix_collection.collisionEnergy), E_max = max(s_matrix_collection.collisionEnergy), N = len(s_matrix_collection.collisionEnergy), n = 3), dtype = float) for temperature in temperatures]
         average_rate_arrays = np.array( [s_matrix_collection.thermalAverage(rate_array.sum(axis=len(arg[2].shape)), distribution_array) for distribution_array in distribution_arrays ] )
-        # correct_momentum_transfer_rate_array = s_matrix_collection.getMomentumTransferRateCoefficientVsL(qn.LF1F2(None, None, F1 = 4, MF1 = 4, F2 = 1, MF2 = 1), unit = 'cm**3/s', param_indices = param_indices)
-        # average_momentum_transfer_arrays = np.array( [ s_matrix_collection.thermalAverage(correct_momentum_transfer_rate_array.sum(axis=0), distribution_array) for distribution_array in distribution_arrays ] )
-        average_momentum_transfer_arrays = np.array( [ s_matrix_collection.thermalAverage(momentum_transfer_rate_array.sum(axis=len(arg[2].shape)), distribution_array) for distribution_array in distribution_arrays ] )
+        correct_momentum_transfer_rate_array = momentum_transfer_rate = np.full(momentum_transfer_rate_array.shape, s_matrix_collection.getMomentumTransferRateCoefficientVsL(qn.LF1F2(None, None, F1 = 4, MF1 = 4, F2 = 1, MF2 = 1), unit = 'cm**3/s', param_indices = param_indices) )
+        average_momentum_transfer_arrays = np.array( [ s_matrix_collection.thermalAverage(correct_momentum_transfer_rate_array.sum(len(arg[2].shape)), distribution_array) for distribution_array in distribution_arrays ] )
+        # average_momentum_transfer_arrays = np.array( [ s_matrix_collection.thermalAverage(momentum_transfer_rate_array.sum(axis=len(arg[2].shape)), distribution_array) for distribution_array in distribution_arrays ] )
         probability_arrays = average_rate_arrays / average_momentum_transfer_arrays
         output_state_resolved_probability_arrays = probability_arrays.squeeze()
         probability_arrays = probability_arrays.sum(axis = (1, 2)).squeeze()
