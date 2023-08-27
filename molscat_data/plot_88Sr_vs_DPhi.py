3dd34e7d8f4a908ded16fd7795445ef44a3a1cb9
Maks Zachary Walewski <maks.walewski@gmail.com>
2023-08-27 16:34:13 +0200

    plot_88Sr_vs_DPhi.py: trying to fix

--- molscat_data/plot_88Sr_vs_DPhi.py
+++ molscat_data/plot_88Sr_vs_DPhi.py
@@ -104,7 +104,7 @@ def plot_probability_vs_DPhi(singlet_phases: float | np.ndarray[float], phase_di
     minima = np.array([line.get_xydata() for line in lines])
     print(f'{minima.shape=}')
     # print(minima[-1])
-    print(minima[:,:,1].min(axis=1))
+    print(minima[:,:,1].nanmin(axis=1))
     ax.set_ylim(0,1)
     ax.xaxis.get_major_ticks()[1].label1.set_visible(False)
     ax_chisq.legend(fontsize = 30, loc = 'upper left')
