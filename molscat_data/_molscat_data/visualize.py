71177d8ea9d4e12be75d7cd6776bca382ee7ad52
Maks Zachary Walewski <maks.walewski@gmail.com>
2023-08-30 16:43:08 +0200

    plot_88Sr_ColorMap.py: experiment

--- molscat_data/_molscat_data/visualize.py
+++ molscat_data/_molscat_data/visualize.py
@@ -693,7 +693,7 @@ class ContourMap:
     def _initiate_plot(figsize = (5.25, 5), dpi=300):
         fig = plt.figure(figsize=figsize, dpi=dpi)
         ax = fig.add_subplot(1,105,(1,100))
-        ax_bar = fig.add_subplot(1,100,(100,105))
+        ax_bar = fig.add_subplot(1,105,(100,105))
         return fig, ax, ax_bar
     
     @classmethod
