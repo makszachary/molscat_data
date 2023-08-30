0eb1708c2f1e21ff255fd7172fe00fd428c8abdb
Maks Zachary Walewski <maks.walewski@gmail.com>
2023-08-30 17:14:09 +0200

    plot_88Sr_ColorMap.py: experiment

--- molscat_data/plot_88Sr_ColorMap.py
+++ molscat_data/plot_88Sr_ColorMap.py
@@ -62,7 +62,8 @@ def plotColorMap(singlet_phases: float | np.ndarray[float], triplet_phases: floa
     png_path.parent.mkdir(parents = True, exist_ok = True)
 
     fig, ax, ax_bar, bar = ContourMap.plotMap(singlet_phases, triplet_phases, theory, n_levels=3)
-    # fig, ax, bar = ContourMap.plotMap(singlet_phases, triplet_phases, theory, n_levels=3)
+    ax.set_xlabel(f'$\\Phi_\\mathrm{{s}}$', fontsize = 24)
+    ax.set_ylabel(f'$\\Phi_\\mathrm{{t}}$', fontsize = 24)
 
     bar.ax.axhspan(experiment-std, experiment+std, color = '0.8', alpha=0.8)
     bar.ax.axhline(experiment, color = '1.0', linestyle = '-', linewidth = 2)
