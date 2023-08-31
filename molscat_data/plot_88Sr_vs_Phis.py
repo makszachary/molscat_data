0c77a619f3969bd404d90b82e08daf15655c4412
Maks Zachary Walewski <maks.walewski@gmail.com>
2023-08-31 19:43:01 +0200

    plot_88Sr_vs_Phis.py: experiment

--- molscat_data/plot_88Sr_vs_Phis.py
+++ molscat_data/plot_88Sr_vs_Phis.py
@@ -82,8 +82,9 @@ def plotPeffVsPhis(singlet_phases: float | np.ndarray[float], phase_differences:
     PhaseTicks.setInMultiplesOfPhi(ax0.xaxis)
 
     # get the positions of the maxima
-    filter_max_arr = np.equal(np.full_like(theory, np.amax(theory, axis = 0)), theory) 
-    coords_vs_phase_difference = tuple( (phase_difference, singlet_phases[filter_max_arr[:,i]], theory[:,i][filter_max_arr[:,i]]) for i, phase_difference in enumerate(phase_differences) )
+    # filter_max_arr = np.equal(np.full_like(theory, np.amax(theory, axis = 0)), theory)
+    max_indices = np.argmax(theory, axis = 0)
+    coords_vs_phase_difference = tuple( (phase_difference, singlet_phases[max_indices[i]], theory[:,i][max_indices[i]]) for i, phase_difference in enumerate(phase_differences) )
     print(coords_vs_phase_difference)
 
     # label the curves with the values of the phase difference near the maxima
