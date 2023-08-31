b860967b7d5844de4ef0757ee80d7cd3610e7074
Maks Zachary Walewski <maks.walewski@gmail.com>
2023-08-31 21:59:15 +0200

    plot_88Sr_ColorMap.py: experiment with subplot placing

--- molscat_data/plot_88Sr_ColorMap.py
+++ molscat_data/plot_88Sr_ColorMap.py
@@ -71,10 +71,10 @@ def plotColorMap(singlet_phases: float | np.ndarray[float], triplet_phases: floa
     bar.ax.axhline(experiment, color = '1.0', linestyle = '-', linewidth = 2)
     labels = [w.get_text() for w in bar.ax.get_yticklabels()]
     locs=list(bar.ax.get_yticks())
-    print(labels, locs)
+    # print(labels, locs)
     labels.append(f'$p_\\mathrm{{eff}}^\\mathrm{{exp}}$')
-    locs.append(exp_cold_lower)
-    print(labels, locs)
+    locs.append(experiment)
+    # print(labels, locs)
     bar.set_ticks(locs)
     bar.set_ticklabels(labels)
     bar.ax.axhline(plot_temperature, color = '0.', linestyle = '-', linewidth = 4)
