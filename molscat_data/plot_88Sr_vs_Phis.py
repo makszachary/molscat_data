0720afe03ffe426064b8097909a31e64af2ff472
Maks Zachary Walewski <maks.walewski@gmail.com>
2023-08-31 18:52:18 +0200

    plot_88Sr_vs_Phis.py: experiment

--- molscat_data/plot_88Sr_vs_Phis.py
+++ molscat_data/plot_88Sr_vs_Phis.py
@@ -109,7 +109,7 @@ def plotPeffVsPhis(singlet_phases: float | np.ndarray[float], phase_differences:
     
     ax1_bar = fig.add_subplot()
     ax1_bar.set_position(gs[int(1000*(lim0[1]-lim0[0]))+1:,95:].get_position(fig))
-    ax1_bar.set_subplotspec(int(1000*(lim0[1]-lim0[0]))+1:gs[0,95:])
+    ax1_bar.set_subplotspec(gs[int(1000*(lim0[1]-lim0[0]))+1:,95:])
 
     # ax0.set_ylim(min([*lim0, *lim1]), max([*lim0, *lim1]))
     # ax1.set_ylim(min([*lim0, *lim1]), max([*lim0, *lim1]))
