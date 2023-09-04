698173bd53f02310cd4e88e82a6c2b63f53c4516
Maks Zachary Walewski <maks.walewski@gmail.com>
2023-09-04 17:06:44 +0200

    run_spin_exchange_vs_B.py: fix

--- molscat_data/plot_spin_exchange_vs_B.py
+++ molscat_data/plot_spin_exchange_vs_B.py
@@ -161,8 +161,8 @@ def plot_probability_vs_B(phases: tuple[tuple[float, float], ...], phases_distin
     # fig.tight_layout()
     fig.subplots_adjust(left = 0.03, top = 0.9, right = 0.96, bottom = 0.2, hspace = .0)
     # fig.tight_layout()
-    fig.savefig(png_path, bbox_inches='tight')#, pad_inches = 0)
-    fig.savefig(svg_path, bbox_inches='tight')#, pad_inches = 0)
+    fig.savefig(png_path, bbox_inches='tight', pad_inches = 0.)
+    fig.savefig(svg_path, bbox_inches='tight', pad_inches = 0.)
     plt.close()
 
 def main():
