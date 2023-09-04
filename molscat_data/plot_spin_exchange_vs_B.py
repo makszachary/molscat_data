22adbdd47ce72c0c6e8b92c0bca2568ff6087801
Maks Zachary Walewski <maks.walewski@gmail.com>
2023-09-04 16:39:23 +0200

    run_spin_exchange_vs_B.py: fix

--- molscat_data/plot_spin_exchange_vs_B.py
+++ molscat_data/plot_spin_exchange_vs_B.py
@@ -156,10 +156,10 @@ def plot_probability_vs_B(phases: tuple[tuple[float, float], ...], phases_distin
     ax1_bar.tick_params(axis = 'both', labelsize = 10)
     ax1_bar.get_yaxis().labelpad = 4
     ax1_bar.set_ylabel('$T\\,(\\mathrm{mK})$', rotation = 0, fontsize = 10)
-    ax1_bar.yaxis.set_label_coords(2.1, 1.1)
+    ax1_bar.yaxis.set_label_coords(2.1, 1.08)
 
     # fig.tight_layout()
-    fig.subplots_adjust(left = 0.15, top = 0.98, right = 0.9, bottom = 0.15, hspace = .0)
+    fig.subplots_adjust(left = 0.15, top = 0.95, right = 0.9, bottom = 0.15, hspace = .0)
     fig.savefig(png_path, bbox_inches='tight', pad_inches = 0)
     fig.savefig(svg_path, bbox_inches='tight', pad_inches = 0)
     plt.close()
