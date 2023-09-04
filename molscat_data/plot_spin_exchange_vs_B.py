73d3213d42955a3794970c564a0f3af53e18f192
Maks Zachary Walewski <maks.walewski@gmail.com>
2023-09-04 17:42:34 +0200

    run_spin_exchange_vs_B.py: fix

--- molscat_data/plot_spin_exchange_vs_B.py
+++ molscat_data/plot_spin_exchange_vs_B.py
@@ -90,7 +90,7 @@ def plot_probability_vs_B(phases: tuple[tuple[float, float], ...], phases_distin
     dpi = 1000
     # fig, ax0 = ValuesVsModelParameters.plotValues(magnetic_fields, theory, experiment, std, theory_distinguished, theory_colors, theory_distinguished_colors, figsize=figsize, dpi=dpi)
     fig, ax0 = ValuesVsModelParameters.plotValues(magnetic_fields, theory, experiment=None, std=None, theory_distinguished=None, theory_colors=theory_colors, theory_distinguished_colors=theory_distinguished_colors, figsize=figsize, dpi=dpi)
-    ax0.scatter([magnetic_field_experimental,], experiment, s = 16, c = 'k', marker = 'd')
+    ax0.scatter([magnetic_field_experimental,], experiment, s = 8, c = 'k', marker = 'd')
     # print(std)
     ax0.errorbar([magnetic_field_experimental, ], experiment, std, ecolor = 'k', capsize = 4)
     ax0.set_ylim(0, ax0.get_ylim()[1])
