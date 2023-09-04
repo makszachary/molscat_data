7d27df8b7b38a4346573174ef6a9cf7e11b13828
Maks Zachary Walewski <maks.walewski@gmail.com>
2023-09-04 23:15:25 +0200

    plot_88Sr_ColorMap_and_Sections.py modified

--- molscat_data/plot_88Sr_ColorMap_and_Sections.py
+++ molscat_data/plot_88Sr_ColorMap_and_Sections.py
@@ -134,6 +134,7 @@ def plotColorMapAndSections(singlet_phases: float | np.ndarray[float], triplet_p
     theory_distinguished = np.moveaxis(np.array( [ arrays_cold_lower_distinguished[:,T_index, 0], ]), 0, -1)
 
     fig1_ax1 = ValuesVsModelParameters.plotValuestoAxis(fig1_ax1, singlet_phases, theory, experiment, std, theory_distinguished, theory_colors, theory_distinguished_colors)
+    PhaseTicks.linearStr(fig1_ax1.yaxis, 0.2, 0.1, '${x:.1f}$')
 
     # draw the label for the experimental value in the upper plot
     fig1_ax0_right = fig1_ax0.twinx()
