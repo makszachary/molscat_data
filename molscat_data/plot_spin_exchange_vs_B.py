28498b9cdc3267bffe23b810aab69fb56ed6f2a5
Maks Zachary Walewski <maks.walewski@gmail.com>
2023-09-05 13:52:49 +0200

    plot_spin_exchange_vs_B.py: changed color for distinguished temperature

--- molscat_data/plot_spin_exchange_vs_B.py
+++ molscat_data/plot_spin_exchange_vs_B.py
@@ -92,7 +92,7 @@ def plot_probability_vs_B(phases: tuple[tuple[float, float], ...], phases_distin
     dpi = 1000
     # fig, ax0 = ValuesVsModelParameters.plotValues(magnetic_fields, theory, experiment, std, theory_distinguished, theory_colors, theory_distinguished_colors, figsize=figsize, dpi=dpi)
     fig, ax0 = ValuesVsModelParameters.plotValues(magnetic_fields, theory, experiment=None, std=None, theory_distinguished=None, theory_formattings = theory_formattings, theory_distinguished_formattings=theory_distinguished_formattings, figsize=figsize, dpi=dpi)
-    ax0.scatter([magnetic_field_experimental,], experiment, s = 14, c = theory_distinguished_formattings[0]['color'], marker = 'd')
+    ax0.scatter([magnetic_field_experimental,], experiment, s = 16, c = theory_distinguished_formattings[0]['color'], marker = 'd')
     # print(std)
     ax0.errorbar([magnetic_field_experimental, ], experiment, std, ecolor = theory_distinguished_formattings[0]['color'], capsize = 6)
     ax0.set_ylim(0, ax0.get_ylim()[1])
