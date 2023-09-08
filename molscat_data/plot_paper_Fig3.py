71f4f1a91fdaa32a00036230e6268b234206de08
Maks Zachary Walewski <maks.walewski@gmail.com>
2023-09-08 12:17:50 +0200

    plot_paper_Fig3.py modified

--- molscat_data/plot_paper_Fig3.py
+++ molscat_data/plot_paper_Fig3.py
@@ -325,7 +325,7 @@ def plotFig3(singlet_phases: float | np.ndarray[float], triplet_phases: float | 
     fig2_ax.text(0., second_row_height/total_height, f'c', fontsize = 7, family = 'sans-serif', va = 'top', ha = 'left', transform = fig.transFigure, fontweight = 'bold')
     fig3_axs[0].text(0.67, second_row_height/total_height, f'd', fontsize = 7, family = 'sans-serif', va = 'top', ha = 'left', transform = fig.transFigure, fontweight = 'bold')
 
-    fig0.subplots_adjust(left = 0.)
+    fig0.subplots_adjust(left = 0.02)
     fig1.subplots_adjust(left = 0.17, right = 0.97, hspace = .0)
     fig2.subplots_adjust(left = 0.1, right = 0.97)
     fig3.subplots_adjust(left = 0.17, right = 1-(0.03)*90/60)
