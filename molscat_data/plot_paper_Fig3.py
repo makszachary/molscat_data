c1c70368f175a21e14155b43c3eb8008ca489c50
Maks Zachary Walewski <maks.walewski@gmail.com>
2023-09-08 18:18:46 +0200

    plot_paper_Fig3.py: experiments

--- molscat_data/plot_paper_Fig3.py
+++ molscat_data/plot_paper_Fig3.py
@@ -331,7 +331,7 @@ def plotFig3(singlet_phases: float | np.ndarray[float], triplet_phases: float | 
     fig3_axs[0].text(0.67, second_row_height/total_height, f'd', fontsize = 7, family = 'sans-serif', va = 'top', ha = 'left', transform = fig.transFigure, fontweight = 'bold')
 
     fig0.subplots_adjust(left = 0.05)
-    fig1.subplots_adjust(left = 0.17, right = 0.97, hspace = 0)
+    fig1.subplots_adjust(left = 0.17, right = 0.97, bottom = 0.1, hspace = 0)
     fig2.subplots_adjust(left = 0.1, right = 0.97)
     fig3.subplots_adjust(left = 0.17, right = 1-(0.03)*90/60, hspace = 0)
 
