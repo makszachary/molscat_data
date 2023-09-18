6b1b31faa3a0cbe1af705828e5e213ca5b79ee29
Maks Zachary Walewski <maks.walewski@gmail.com>
2023-09-18 15:30:22 +0200

    plot_paper_Fig2.py: changes in colours

--- molscat_data/plot_paper_Fig2.py
+++ molscat_data/plot_paper_Fig2.py
@@ -452,7 +452,7 @@ def plotPeffAverageVsMassToFig(fig, singlet_phase: float, triplet_phase: float, 
     PhaseTicks.linearStr(fig_ax.xaxis, 1, 0.2, '${x:n}$')
 
     for i, curve_name in enumerate(reversed(curves_names)):
-        fig_ax.get_lines()[-i].set_label(curve_name)
+        fig_ax.get_lines()[-(i+2)].set_label(curve_name)
     labelLines(fig_ax.get_lines(), align = False, outline_width=2, color = 'white', fontsize = matplotlib.rcParams["xtick.labelsize"], )
     labelLines(fig_ax.get_lines(), align = False, outline_color = None, yoffsets= -6.7e-3*(fig_ax.get_ylim()[1]-fig_ax.get_ylim()[0]), fontsize = matplotlib.rcParams["xtick.labelsize"], )
     # props = dict(boxstyle='round', facecolor='none', edgecolor='midnightblue')
