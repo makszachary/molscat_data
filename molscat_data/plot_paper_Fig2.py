5e5c9b4cc994829d6c01db7210886ca741db64c2
Maks Zachary Walewski <maks.walewski@gmail.com>
2023-09-18 15:43:31 +0200

    plot_paper_Fig2.py: changes in colours

--- molscat_data/plot_paper_Fig2.py
+++ molscat_data/plot_paper_Fig2.py
@@ -455,7 +455,7 @@ def plotPeffAverageVsMassToFig(fig, singlet_phase: float, triplet_phase: float, 
         # [print(f'{i}, {line.get_xydata()=}') for i, line in enumerate(fig_ax.get_lines())]
         if len(fig_ax.get_lines()[i+2*3].get_xydata()) > 0: fig_ax.get_lines()[i+2*3].set_label(curve_name) 
     labelLines(fig_ax.get_lines(), align = False, outline_width=2, color = 'white', fontsize = matplotlib.rcParams["xtick.labelsize"], )
-    labelLines(fig_ax.get_lines(), align = False, outline_color = None, yoffsets= -6.7e-3*(fig_ax.get_ylim()[1]-fig_ax.get_ylim()[0]), fontsize = matplotlib.rcParams["xtick.labelsize"], )
+    labelLines(fig_ax.get_lines(), align = False, outline_color = None, yoffsets= -1/2*6.7e-3*(fig_ax.get_ylim()[1]-fig_ax.get_ylim()[0]), fontsize = matplotlib.rcParams["xtick.labelsize"], )
     # props = dict(boxstyle='round', facecolor='none', edgecolor='midnightblue')
     # fig_ax.text(0.03, 0.10, f'$\\Delta\\Phi_\\mathrm{{fit}} = {(magnetic_phases[0][1]-magnetic_phases[0][0])%1:.2f}\\pi$', va = 'center', ha = 'left', transform = fig_ax.transAxes, bbox = props)
 
