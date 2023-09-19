d050e739b9ab9848cd4b0a6e1d3fa4671fbaf543
Maks Zachary Walewski <maks.walewski@gmail.com>
2023-09-19 18:15:20 +0200

    plot_paper_Fig2.py: changed formatting

--- molscat_data/plot_paper_Fig2.py
+++ molscat_data/plot_paper_Fig2.py
@@ -454,7 +454,7 @@ def plotFig2(singlet_phase: float, triplet_phase: float, so_scaling: float, redu
     for i, lbl in enumerate(lines_labels):
         fig2_ax.get_lines()[i].set_label(lbl) 
     xvals = (fig2_ax.get_xlim()[0] + 0.2*(fig2_ax.get_xlim()[1]-fig2_ax.get_xlim()[0]), fig2_ax.get_xlim()[0] + 0.8*(fig2_ax.get_xlim()[1]-fig2_ax.get_xlim()[0]))
-    xvals = 2e-3
+    # xvals = 2e-3
     labelLines(fig2_ax.get_lines(), align = False, outline_width=8, color = 'white', fontsize = matplotlib.rcParams["xtick.labelsize"], )
     labelLines(fig2_ax.get_lines(), align = False, outline_color = None, yoffsets= -0*6.7e-3*(fig2_ax.get_ylim()[1]-fig2_ax.get_ylim()[0]), fontsize = matplotlib.rcParams["xtick.labelsize"], )
 
