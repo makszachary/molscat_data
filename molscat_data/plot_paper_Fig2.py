a59d5a1368e50d0239f01915e5cc6004d507e760
makszachary <mz.walewski@student.uw.edu.pl>
2023-10-30 15:52:57 +0100

    plot_paper_Fig2.py: experimental modification of the barplot

--- molscat_data/plot_paper_Fig2.py
+++ molscat_data/plot_paper_Fig2.py
@@ -105,6 +105,10 @@ def plotFig2(singlet_phase: float, triplet_phase: float, so_scaling: float, redu
     bars_formatting_hpf = { 'facecolor': 'indianred', 'edgecolor': 'black', 'alpha': 0.9, 'ecolor': 'black', 'capsize': 3 }
     exp_bars_formatting_hpf = { 'facecolor': 'firebrick', 'edgecolor': 'black', 'alpha': 0.9, 'ecolor': 'black', 'capsize': 3 }
     SE_bars_formatting_hpf = { 'facecolor': 'firebrick', 'edgecolor': 'black', 'alpha': 0.9, 'ecolor': 'black', 'capsize': 3 }
+    bars_formatting_cold_higher = { 'facecolor': 'royalblue', 'edgecolor': 'black', 'alpha': 0.9, 'ecolor': 'black', 'capsize': 3 }
+    exp_bars_formatting_cold_higher = { 'facecolor': 'midnightblue', 'edgecolor': 'black', 'alpha': 0.9, 'ecolor': 'black', 'capsize': 3 }
+    SE_bars_formatting_cold_higher = { 'facecolor': 'midnightblue', 'edgecolor': 'black', 'alpha': 0.9, 'ecolor': 'black', 'capsize': 3 }
+
     # fig0_ax = Barplot.plotBarplotToAxes(fig0_ax, theory_hpf, experiment_hpf, std_hpf, barplot_labels, theory_SE_hpf, bars_formatting = bars_formatting_hpf, exp_bars_formatting = exp_bars_formatting_hpf, SE_bars_formatting = SE_bars_formatting_hpf)
     theory = [ theory_hpf, theory_cold_higher ]
     theory_SE = [ theory_SE_hpf, theory_SE_cold_higher ]
