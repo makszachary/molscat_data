167db64868ecb32ddd1505a8dd9f06222e49b5ec
Maks Zachary Walewski <maks.walewski@gmail.com>
2023-11-02 10:04:19 +0100

    .\plot_paper_Fig2_concise.py: small mods

--- molscat_data/plot_paper_Fig2_concise.py
+++ molscat_data/plot_paper_Fig2_concise.py
@@ -229,7 +229,7 @@ def plotPeffAverageVsMassToFig(fig, singlet_phase: float, triplet_phase: float, 
     # fig_ax.set_xticks([ 43.0, 43.5, *reduced_masses_experimental], labels = [ '$43.0$', '$43.5$', f'${{}}^{{84}}\\mathrm{{Sr^+}}$', f'${{}}^{{86}}\\mathrm{{Sr^+}}$', f'${{}}^{{87}}\\mathrm{{Sr^+}}$', f'${{}}^{{88}}\\mathrm{{Sr^+}}$' ])
     xx_text, yy_text, alignments = reduced_masses_experimental, experiment, [ {'ha': 'center', 'va': 'top'}, {'ha': 'center', 'va': 'top'}, {'ha': 'right', 'va': 'baseline'}, {'ha': 'right', 'va': 'baseline'} ]
     xx_text[[2,3]] -= 0.05
-    yy_text[[0,1]] -= -0.15
+    yy_text[[0,1]] -= 0
     yy_text[[2,3]] += 0.04
     for i in [0, 1, 3]:
         fig_ax.text(xx_text[i], yy_text[i], reduced_masses_labels[i], color = 'firebrick', fontsize = 'x-small', **alignments[i])
