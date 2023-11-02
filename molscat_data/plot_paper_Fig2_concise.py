c7a02044591c42d36027c1d852902b569a3af31f
Maks Zachary Walewski <maks.walewski@gmail.com>
2023-11-02 10:01:35 +0100

    .\plot_paper_Fig2_concise.py: small mods

--- molscat_data/plot_paper_Fig2_concise.py
+++ molscat_data/plot_paper_Fig2_concise.py
@@ -227,13 +227,13 @@ def plotPeffAverageVsMassToFig(fig, singlet_phase: float, triplet_phase: float, 
     PhaseTicks.linearStr(fig_ax.yaxis, 0.1, 0.05, '${x:.1f}$')
     PhaseTicks.linearStr(fig_ax.xaxis, 0.5, 0.1, '${x:.1f}$')
     # fig_ax.set_xticks([ 43.0, 43.5, *reduced_masses_experimental], labels = [ '$43.0$', '$43.5$', f'${{}}^{{84}}\\mathrm{{Sr^+}}$', f'${{}}^{{86}}\\mathrm{{Sr^+}}$', f'${{}}^{{87}}\\mathrm{{Sr^+}}$', f'${{}}^{{88}}\\mathrm{{Sr^+}}$' ])
-    xx_text, yy_text = reduced_masses_experimental, experiment
-    xx_text[[2,3]] -= 0.15
-    yy_text[[0,1]] -= -0.05
+    xx_text, yy_text, alignments = reduced_masses_experimental, experiment, [ {'ha': 'center', 'va': 'top'}, {'ha': 'center', 'va': 'top'}, {'ha': 'right', 'va': 'baseline'}, {'ha': 'right', 'va': 'baseline'} ]
+    xx_text[[2,3]] -= 0.05
+    yy_text[[0,1]] -= -0.15
     yy_text[[2,3]] += 0.04
-    for x, y, label in zip(xx_text[[0,1,3]], yy_text[[0,1,3]], reduced_masses_labels[0:2]+[reduced_masses_labels[3],]):
-        fig_ax.text(x, y, label, color = 'firebrick', fontsize = 'x-small', ha = 'center', va = 'top')
-    fig_ax.text(xx_text[2], yy_text[2], reduced_masses_labels[2], color = 'darkmagenta', fontsize = 'x-small', ha = 'center', va = 'baseline')
+    for i in [0, 1, 3]:
+        fig_ax.text(xx_text[i], yy_text[i], reduced_masses_labels[i], color = 'firebrick', fontsize = 'x-small', **alignments[i])
+    fig_ax.text(xx_text[2], yy_text[2], reduced_masses_labels[2], color = 'darkmagenta', fontsize = 'x-small', **alignments[i])
 
     for i, curve_name in enumerate(curves_names):
         # [print(f'{i}, {line.get_xydata()=}') for i, line in enumerate(fig_ax.get_lines())]
