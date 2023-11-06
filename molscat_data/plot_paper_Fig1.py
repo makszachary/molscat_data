89fb8fa18a7320671d7c0d43e7f7f34969327356
makszachary <mz.walewski@student.uw.edu.pl>
2023-11-06 14:08:16 +0100

    plot_paper_Fig1.py: added p_SR vs so_Scaling

--- molscat_data/plot_paper_Fig1.py
+++ molscat_data/plot_paper_Fig1.py
@@ -182,7 +182,8 @@ def plotPeffVsSOScalingToAxis(ax, so_scaling_values, singlet_phase, triplet_phas
     experiment = np.array( [ exp_hot[0,0], ] )
     std = np.array( [ exp_hot[1,0], ] )
 
-    xx = np.full((len(so_scaling_values), 1), so_scaling_values).transpose()
+    # xx = np.full((len(so_scaling_values), 1), so_scaling_values).transpose()
+    xx = np.array(so_scaling_values)
     T_index = np.nonzero(temperatures == plot_temperature)[0][0]
     theory = np.moveaxis(np.array( [ arrays_hot[:,T_index,0], ] ), 0, -1)
     theory_distinguished = theory
