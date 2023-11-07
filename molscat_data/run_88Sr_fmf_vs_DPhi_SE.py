8119c8544759b28ad2260b6e37c34d0b76b57fc0
makszachary <mz.walewski@student.uw.edu.pl>
2023-11-07 16:39:40 +0100

    run_88Sr_fmf_vs_DPhi_SE.py: trying to fix a bug

--- molscat_data/run_88Sr_fmf_vs_DPhi_SE.py
+++ molscat_data/run_88Sr_fmf_vs_DPhi_SE.py
@@ -301,9 +301,9 @@ def main():
     nenergies, E_min, E_max, n = args.nenergies, args.E_min, args.E_max, args.n_grid
     energy_tuple = tuple( round(n_root_scale(i, E_min, E_max, nenergies-1, n = n), sigfigs = 11) for i in range(nenergies) )
 
-    singlet_phase = default_singlet_phase_function(1.0) if args.singlet_phase is None else args.singlet_phase
-    if args.phase_step is None:
-        triplet_phases = (default_triplet_phase_function(1.0),) if args.triplet_phase is None else (args.triplet_phase,)
+    singlet_phase = default_singlet_phase_function(1.0) if (args.singlet_phase is None or args.singlet_phase == 'None') else args.singlet_phase
+    if (args.phase_step is None or args.singlet_phase == 'None'):
+        triplet_phases = (default_triplet_phase_function(1.0),) if (args.triplet_phase is None or args.singlet_phase == 'None') else (args.triplet_phase,)
     else:
         triplet_phases = np.array([( singlet_phase + phase_difference ) % 1 for phase_difference in np.arange(0, 1., args.phase_step) if (singlet_phase + phase_difference ) % 1 != 0 ] ).round(decimals=4)
     phases = np.around(tuple((singlet_phase, triplet_phase) for triplet_phase in triplet_phases), decimals = 4)
