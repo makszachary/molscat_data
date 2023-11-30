1c5287cdfe79487a69d1c5bc25bf783babd7b0dd
makszachary <mz.walewski@student.uw.edu.pl>
2023-11-30 13:15:34 +0100

    plot_paper_Fig1.py: small changes in controlling temperatures

--- molscat_data/plot_paper_Fig1.py
+++ molscat_data/plot_paper_Fig1.py
@@ -55,7 +55,7 @@ def plotFig1(singlet_phases: float | np.ndarray[float], phase_differences: np.nd
     cm = 1/2.54
     ws, hs = 0.05, 0.05
     nrows = 2
-    row_height = 3.75
+    row_height = 3.8
     vpad = 1
     total_height = nrows*row_height + (nrows-1)*vpad
     figsize = (18*cm, total_height*cm)
@@ -86,7 +86,7 @@ def plotFig1(singlet_phases: float | np.ndarray[float], phase_differences: np.nd
     figs_axes[0][0].text(0., 1.0, f'a', fontsize = 8, family = 'sans-serif', va = 'top', ha = 'left', transform = fig.transFigure, fontweight = 'bold')
     figs_axes[0][0].text(0.35, 1.0, f'b', fontsize = 8, family = 'sans-serif', va = 'top', ha = 'left', transform = fig.transFigure, fontweight = 'bold')
     figs_axes[0][0].text(0.35, 5.7/8.5, f'c', fontsize = 8, family = 'sans-serif', va = 'top', ha = 'left', transform = fig.transFigure, fontweight = 'bold')
-    figs_axes[0][0].text(0.35, 2.6/8.5, f'd', fontsize = 8, family = 'sans-serif', va = 'top', ha = 'left', transform = fig.transFigure, fontweight = 'bold')
+    figs_axes[0][0].text(0.35, 2.7/8.5, f'd', fontsize = 8, family = 'sans-serif', va = 'top', ha = 'left', transform = fig.transFigure, fontweight = 'bold')
     figs_axes[1][0].text(0.7, 1.0, f'e', fontsize = 8, family = 'sans-serif', va = 'top', ha = 'left', transform = fig.transFigure, fontweight = 'bold')
     figs_axes[2][0].text(0.7, row_height/total_height + 0.04, f'f', fontsize = 8, family = 'sans-serif', va = 'top', ha = 'left', transform = fig.transFigure, fontweight = 'bold')
 
