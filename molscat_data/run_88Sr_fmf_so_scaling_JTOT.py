5bbe115465a099a383641bb64ff144ca1bcf0816
makszachary <mz.walewski@student.uw.edu.pl>
2024-01-25 12:16:12 +0100

    run_88Sr_fmf_so_scaling_JTOT.py: struggling with shutil.rmtree

--- molscat_data/run_88Sr_fmf_so_scaling_JTOT.py
+++ molscat_data/run_88Sr_fmf_so_scaling_JTOT.py
@@ -163,7 +163,7 @@ def create_and_run_parallel(molscat_input_templates, singlet_phase, triplet_phas
             output_dirs.append( output_path.parent )
             print(f"It took {duration:.2f} s to create the molscat input: {input_path}, run molscat and generate the output: {output_path}.")
     
-    for dir_path in [*input_dirs, *scaled_so_dirs, *output_dirs]:
+    for dir_path in np.unique([*input_dirs, *scaled_so_dirs, *output_dirs]):
         print(dir_path)
         # _zip(dir_path, delete_after = True)
         zip_name = dir_path.name + '_ARCHIVE'
