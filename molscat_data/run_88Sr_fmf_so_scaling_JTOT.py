690504d8b66a25a42f826f6c1f33fbe7f9a55e4b
makszachary <mz.walewski@student.uw.edu.pl>
2024-01-25 13:01:26 +0100

    run_88Sr_fmf_so_scaling_JTOT.py: struggling with shutil.rmtree

--- molscat_data/run_88Sr_fmf_so_scaling_JTOT.py
+++ molscat_data/run_88Sr_fmf_so_scaling_JTOT.py
@@ -164,13 +164,8 @@ def create_and_run_parallel(molscat_input_templates, singlet_phase, triplet_phas
             print(f"It took {duration:.2f} s to create the molscat input: {input_path}, run molscat and generate the output: {output_path}.")
     
     for dir_path in np.unique([*input_dirs, *scaled_so_dirs, *output_dirs]):
-        print(dir_path)
-        # _zip(dir_path, delete_after = True)
-        zip_name = dir_path.name + '_ARCHIVE'
-        zip_path = dir_path.parent / zip_name
-        # zip_path = dir_path.parent / (zip_name + '.zip')
+        # zip_path = dir_path.parent / (dir_path.name + '.zip')
         shutil.make_archive(dir_path, 'zip', dir_path)
-        ## problematic line below!!!!!!!!!!!!!!!!!!!!!!!!
         shutil.rmtree(dir_path,)
 
     t1 = time.perf_counter()
