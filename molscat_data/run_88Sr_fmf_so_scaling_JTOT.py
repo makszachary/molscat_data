52620ab8f8cd01f441439e4f39598904a4afc375
Maks Zachary Walewski <maks.walewski@gmail.com>
2024-01-27 17:24:37 +0100

    run_88Sr_fmf_so_scaling_JTOT.py: small fix

--- molscat_data/run_88Sr_fmf_so_scaling_JTOT.py
+++ molscat_data/run_88Sr_fmf_so_scaling_JTOT.py
@@ -409,7 +409,7 @@ def main():
         # pickle_paths = np.unique(pickle_paths)
 
         transfer_output_dir = scratch_path / 'molscat' / 'outputs' / args.transfer_input_dir_name / f'{E_min:.2e}_{E_max:.2e}_{transfer_nenergies}_E' / f'{singlet_phase:.4f}_{triplet_phase:.4f}' / f'{0.0:.4f}' / f'in_4_4_1_1'
-        _, duration, output_dir, transfer_pickle_path = collect_and_pickle( transfer_output_dir, singlet_phase, triplet_phase, None, energy_tuple, )
+        _, duration, output_dir, transfer_pickle_path = collect_and_pickle( transfer_output_dir, singlet_phase, triplet_phase, None, transfer_energy_tuple, )
         print(f"The time of gathering the outputs from {transfer_output_dir} into SMatrix object and pickling SMatrix into the file: {transfer_pickle_path} was {duration:.2f} s.")
 
     if args.calc:
