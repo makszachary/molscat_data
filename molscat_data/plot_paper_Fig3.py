fda14e7338b717eff18cfbf65898e3fcba6a3b39
makszachary <mz.walewski@student.uw.edu.pl>
2024-02-07 12:01:50 +0100

    plot_paper_Fig3.py: debugging

--- molscat_data/plot_paper_Fig3.py
+++ molscat_data/plot_paper_Fig3.py
@@ -137,7 +137,9 @@ def plotColorMapAndSectionstoFigs(fig0, fig1, singlet_phases: float | np.ndarray
     T_index = np.nonzero(temperatures == plot_temperature)[0][0]
     theory = arrays_cold_lower[:,:,T_index,0]
     print(theory)
-    print(np.isnan(theory))
+    print(f'{np.isnan(theory) =}')
+    print(f'{theory[np.isnan(theory)][-1::] =}')
+    print(f'{theory[np.isnan(theory)][1::] =}')
     theory[np.isnan(theory)] = (theory[np.isnan(theory)][1::]+theory[np.isnan(theory)][-1::])/2
     print(theory)
     theory_distinguished = np.moveaxis(np.array( [ arrays_cold_lower_distinguished[:,T_index, 0], ]), 0, -1)
