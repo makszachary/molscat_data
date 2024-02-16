d2d91a54db973f7fa8154ec13865be47708baebd
makszachary <mz.walewski@student.uw.edu.pl>
2024-02-16 15:36:43 +0100

    plot_paper_Fig2_concise.py: added writing chi-squared from the barplot to log file

--- molscat_data/plot_paper_Fig2_concise.py
+++ molscat_data/plot_paper_Fig2_concise.py
@@ -166,8 +166,9 @@ def plotFig2(singlet_phase: float, triplet_phase: float, so_scaling: float, redu
     figs_axes[0][0].legend(handles, labels, handler_map = hmap, loc = 'upper right', bbox_to_anchor = (0.98, 1.02), fontsize = 'xx-small', labelspacing = 0.75, frameon=False)
 
     chi_sq = chi_squared(theory, experiment = experiment, std = std)
-    chi_sq_without_22_ss = chi_squared(theory.flatten()[:-1], experiment = experiment.flatten()[:-1], std = std.flatten()[:-1])
-    log_str = f'''For T = {plot_temperature:.2e} K, the chi-squared for ab-initio singlet potential (Phi_s = {singlet_phase:.4f} pi) and DeltaPhi = {triplet_phase-singlet_phase:.2f} calculated from the barplot for 88Sr+ is {chi_sq:.3f}. Excluding the measurement of the cold spin change for |2,2>|up> state, chi-squared is {chi_sq_without_22_ss:.3f}'''
+    chi_sq_without_22_cold = chi_squared(theory.flatten()[:-1], experiment = experiment.flatten()[:-1], std = std.flatten()[:-1])
+    chi_sq_without_22_and_20_cold = chi_squared(theory.flatten()[[0,1,2,3,4,5,6,8]], experiment = experiment.flatten()[[0,1,2,3,4,5,6,8]], std = std.flatten()[[0,1,2,3,4,5,6,8]])
+    log_str = f'''For T = {plot_temperature:.2e} K, the chi-squared for ab-initio singlet potential (Phi_s = {singlet_phase:.4f} pi) and DeltaPhi = {triplet_phase-singlet_phase:.2f} calculated from the barplot for 88Sr+ is {chi_sq:.3f}.\nExcluding the measurement of the cold spin change for |2,2>|up> state, chi-squared is {chi_sq_without_22_cold:.3f}.\nExcluding the cold spin change for |2,0>|up> state as well, chi-squared is {chi_sq_without_22_cold:.3f}'''
     with open(log_path, 'w') as log_file:
         log_file.write(log_str)
 
