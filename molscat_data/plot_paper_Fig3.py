b0d35d4912f7d82127e8831b6adb06f4ab365921
Maks Zachary Walewski <maks.walewski@gmail.com>
2024-02-19 15:33:05 +0100

    plot_paper_Fig3.py: enabled plotting p0 vs magnetic field with so scaling

--- molscat_data/plot_paper_Fig3.py
+++ molscat_data/plot_paper_Fig3.py
@@ -439,7 +439,7 @@ def main():
     else:
         temperatures = np.array(args.temperatures)
 
-    [plotFig3(phase_step_cm = args.phase_step_cm, phase_step_sections = args.phase_step_sections, phase_differences = phase_differences, phase_difference_distinguished = args.phase_difference, so_scaling = so_scaling, magnetic_phases = magnetic_phases, magnetic_fields = magnetic_fields, magnetic_field_experimental = 2.97, MF_in = MF1, MS_in = MF2, energy_tuple = energy_tuple, temperatures = temperatures, plot_temperature = temperature, cm_input_dir_name = args.cm_input_dir_name, vs_B_input_dir_name = args.vs_B_input_dir_name, colormap_hybrid = args.colormap_hybrid, plot_p0 = args.plot_p0, plot_section_lines = args.plot_section_lines, journal_name = args.journal, fmf_colormap = args.fmf_colormap, fmf_vs_B = args.so_scaling_vs_B, plot_nan = args.plot_nan) for temperature in temperatures]
+    [plotFig3(phase_step_cm = args.phase_step_cm, phase_step_sections = args.phase_step_sections, phase_differences = phase_differences, phase_difference_distinguished = args.phase_difference, so_scaling = so_scaling, magnetic_phases = magnetic_phases, magnetic_fields = magnetic_fields, magnetic_field_experimental = 2.97, MF_in = MF1, MS_in = MF2, energy_tuple = energy_tuple, temperatures = temperatures, plot_temperature = temperature, cm_input_dir_name = args.cm_input_dir_name, vs_B_input_dir_name = args.vs_B_input_dir_name, colormap_hybrid = args.colormap_hybrid, plot_p0 = args.plot_p0, plot_section_lines = args.plot_section_lines, journal_name = args.journal, fmf_colormap = args.fmf_colormap, so_scaling_vs_B = args.so_scaling_vs_B, plot_nan = args.plot_nan) for temperature in temperatures]
 
 if __name__ == '__main__':
     main()
