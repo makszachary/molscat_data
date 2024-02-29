545adbb347a87b31d075d94896527a18a1663317
makszachary <mz.walewski@student.uw.edu.pl>
2024-02-29 11:18:18 +0100

    plot_paper_Fig3.py: changed axis labels

--- molscat_data/plot_paper_Fig3.py
+++ molscat_data/plot_paper_Fig3.py
@@ -267,7 +267,7 @@ def plotMagneticFieldtoFigs(fig2, fig3, magnetic_phases: tuple[tuple[float, floa
     theory_colors = list(reversed([color_map(singlet_phase) for singlet_phase, triplet_phase in magnetic_phases]))
     theory_formattings = [ {'color': color, 'linewidth': 2} for color in theory_colors ]
     # theory_distinguished_formattings = [ {'color': 'k', 'linewidth': 4, 'linestyle':  (1.05,(0.1,2)), 'dash_capstyle': 'round' } for exp in experiment]
-    mrkcolor='#DC143C' # '#f5390a'
+    mrkcolor='#fc1456' # '#f5390a'
     theory_distinguished_formattings = [ {'color': 'k', 'linewidth': 0,
                                           'markevery': 0.08, 'markersize': 3,
                                           'marker': 'o', 'markeredgecolor': mrkcolor, 'markerfacecolor': mrkcolor} for exp in experiment]
