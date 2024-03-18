8a9ebbf5e5f0e3a4a851ef0929e502ccabd9bc14
makszachary <mz.walewski@student.uw.edu.pl>
2024-03-18 13:37:08 +0100

    plot_paper_Fig2_concise.py: labels changed

--- molscat_data/plot_paper_Fig2_concise.py
+++ molscat_data/plot_paper_Fig2_concise.py
@@ -152,7 +152,7 @@ def plotFig2(singlet_phase: float, triplet_phase: float, so_scaling: float, redu
     figs_axes[0][0].set_ylabel(ylabel)
 
     labels_and_colors = { 'hyperfine relaxation\n(without & with SO coupling)': SE_bars_formatting_hpf['facecolor'],
-                         'cold spin change\n(without & with SO coupling)': SE_bars_formatting_cold_higher['facecolor'], }
+                         'cold spin flip\n(without & with SO coupling)': SE_bars_formatting_cold_higher['facecolor'], }
     handles_colors = [ plt.Rectangle((0,0), 1, 1, facecolor = labels_and_colors[color_label], edgecolor = 'k', hatch = '' ) for color_label in labels_and_colors.keys() ]
     colors_and_hatches = [ (SE_format['facecolor'], SO_format['facecolor'], '') for SE_format, SO_format in zip(SE_bars_formatting, bars_formatting, strict = True) ]
     labels = [ *list(labels_and_colors.keys()),]
