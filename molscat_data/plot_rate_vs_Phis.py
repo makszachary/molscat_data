a44e9e3cb22fa367559cd1a9fd91b5084620bce5
makszachary <mz.walewski@student.uw.edu.pl>
2024-03-19 13:47:13 +0100

    plot_rate_vs_Phis.py:tried to fix a bug

--- molscat_data/plot_rate_vs_Phis.py
+++ molscat_data/plot_rate_vs_Phis.py
@@ -77,9 +77,8 @@ def plotRateVsPhisForEachEnergy(phase_step: float, phase_difference: float, so_s
 
     k_L_E_array_paths = [  arrays_dir_path / input_dir_name / f'{E_min:.2e}_{E_max:.2e}_{nenergies}_E' / f'{singlet_phase:.4f}_{(singlet_phase+phase_difference)%1:.4f}' / f'{so_scaling:.4f}' / f'in_{F1}_{MF1}_{F2}_{MF2}' / 'k_L_E' / 'cold_lower' / f'OUT_{F1}_{MF1out}_{F2}_{MF2out}_IN_{F1}_{MF1}_{F2}_{MF2}.txt' if ( singlet_phase+phase_difference ) % 1 !=0 else None for singlet_phase in singlet_phases]
     [print(array_path) for array_path in k_L_E_array_paths if (array_path is not None and not array_path.is_file())]
-    print(k_L_E_array_paths)
-    return
-    k_L_E_arrays = np.array([np.loadtxt(array_path) for array_path in k_L_E_array_paths]).transpose(1,2,0)
+    # print(k_L_E_array_paths)
+    k_L_E_arrays = np.array([np.loadtxt(array_path) if (array_path is not None and array_path.is_file()) else np.full((50, nenergies), np.nan) for array_path in k_L_E_array_paths]).transpose(1,2,0)
     total_k_E_Phis_array = k_L_E_arrays.sum(axis = 0)
 
     filter_max_arr = np.equal(np.full_like(k_L_E_arrays.transpose(2,0,1), np.amax(k_L_E_arrays, axis = 2)).transpose(1,2,0), k_L_E_arrays)
