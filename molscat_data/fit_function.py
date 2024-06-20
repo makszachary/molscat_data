f27294428c7461459054ef6d0a2f0b6c932c0f06
makszachary <mz.walewski@student.uw.edu.pl>
2024-06-20 17:11:25 +0200

    fit_function.py expanded

--- molscat_data/fit_function.py
+++ molscat_data/fit_function.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 import numpy as np
-from scipy.optimize import curve_fit
+from scipy.optimize import curve_fit, brute, differential_evolution
 
 from matplotlib import pyplot as plt
 import matplotlib
@@ -23,6 +23,22 @@ def spin_exchange_DISA(reduced_mass, max_spin_exchange, DeltaPhi88, reducedmass8
 def simple_peff_function(reduced_mass, DeltaPhi88):
     return effective_probability(spin_exchange_DISA(reduced_mass, max_spin_exchange = p0(0.45, pmf_array = pmf_array), DeltaPhi88 = DeltaPhi88), pmf_array=pmf_array)
 
+def residuals(f, xdata, ydata, yerr = None, *args, **kwargs):
+    if len(ydata) != len(xdata): raise IndexError("Length of xdata and ydata should be the same.")
+    if yerr is None: yerr = np.ones(ydata.shape)
+    
+    return np.array([(f(xdata[i], *args, **kwargs) - ydata[i]) / yerr[i] for i in range(len(xdata))])
+
+def sum_of_squares(f, xdata, ydata, yerr = None, *args, **kwargs):
+    res = residuals(f, xdata, ydata, yerr, *args, **kwargs)
+    return np.sum(res**2)
+
+def brute_fit(f, xdata, ydata, yerr = None, bounds = None, Ns = 20,):
+    def fun(*args, **kwargs):
+        return sum_of_squares(f, xdata, ydata, yerr, *args, **kwargs)
+    return brute(fun, ranges = bounds, Ns = Ns, full_output = True)
+    
+
 def fit_data(f, xdata, ydata, yerr=None, bounds = None):
     popt, pcov = curve_fit(f, xdata, ydata, sigma=yerr, bounds = bounds)
     perr = np.sqrt(np.diag(pcov))
@@ -36,23 +52,34 @@ def fit_data(f, xdata, ydata, yerr=None, bounds = None):
 def main():
 
     exp_data_path = Path(__file__).parents[1] / 'data' / 'exp_data' / 'isotopes_hpf.dat'
-    exp_data = np.loadtxt(exp_data_path)[:,[0,1,3]]
+    exp_data = np.loadtxt(exp_data_path)#[:,[0,1,3]]
 
     xdata = np.array([red_mass_87Rb_84Sr_amu, red_mass_87Rb_86Sr_amu, red_mass_87Rb_88Sr_amu])
-    ydata = exp_data[0]
-    yerr = exp_data[1]
+    ydata = exp_data[0, [0,1,3]]
+    yerr = exp_data[1, [0,1,3]]
 
-    fitted_params, param_errors, chi_square = fit_data(simple_peff_function, xdata, ydata, yerr, [130*np.pi, 175*np.pi])
+    # print(residuals(simple_peff_function, xdata, ydata, yerr, DeltaPhi88 = 150.22*np.pi))
+    brt_ft = brute_fit(simple_peff_function, xdata, ydata, yerr, bounds = ((130*np.pi, 180*np.pi),), Ns = 500)
+    print( brt_ft ) 
+    print(f'DeltaPhi88 = {brt_ft[0][0]/np.pi:.4f} pi')
+
+    fitted_params, param_errors, chi_square = fit_data(simple_peff_function, xdata, ydata, yerr, bounds=[173*np.pi, 174*np.pi])
     # print(f'fitted_params = {fitted_params[0]:.4f}, {fitted_params[1]/np.pi:.4f}*pi', f'{param_errors = }', f'{chi_square = }')
     print(f'fitted_params = {fitted_params[0]/np.pi:.4f}*pi', f'{param_errors = }', f'{chi_square = }')
     print([(mu, simple_peff_function(mu, *fitted_params)) for mu in xdata])
     print([(mu, exp_data[:,i]) for i, mu in enumerate(xdata)])
 
-    fig, ax = plt.subplots(figsize = (5, 5), dpi = 300)
-    xx = np.arange(min(xdata)-0.1, max(xdata)+0.1, step = 0.01)
+    fig, ax = plt.subplots(figsize = (6, 4.5), dpi = 300)
+    xx = np.arange(42.48, 43.80, step = 0.01)
     ax.plot(xx, simple_peff_function(xx, *fitted_params))
-    ax.scatter(xdata, ydata)
-    ax.errorbar(xdata, ydata, yerr)
+    ax.scatter(xdata, ydata, s = 16, c = 'firebrick', marker = 'x', edgecolors = 'firebrick')
+    ax.errorbar(xdata, ydata, yerr, ecolor = 'firebrick', capsize = 6, linestyle = 'none')
+
+    ax.scatter(red_mass_87Rb_87Sr_amu, exp_data[0,2], s = 16, c = 'darkmagenta', marker = 'x', edgecolors = 'darkmagenta')
+    ax.errorbar(red_mass_87Rb_87Sr_amu, exp_data[0,2], exp_data[1,2], ecolor = 'darkmagenta', capsize = 6, linestyle = 'none')
+
+    ax.set_ylim(0, 0.5)
+
     fig_path = Path(__file__).parents[1] / 'plots' / 'trash' / '1.pdf'
     fig_path.parent.mkdir(parents = True, exist_ok = True)
     plt.savefig(fig_path)
