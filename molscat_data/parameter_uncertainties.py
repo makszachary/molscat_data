d7ab4f98c38f144061133777a63f91321c450b0d
Maks Zachary Walewski <maks.walewski@gmail.com>
2024-06-25 15:40:45 +0200

    parameter_uncertainties.py modified

--- molscat_data/parameter_uncertainties.py
+++ molscat_data/parameter_uncertainties.py
@@ -62,12 +62,13 @@ def get_p0_vs_DPhi(singlet_phases: float | np.ndarray[float], phase_differences:
     experiment = np.array( [ exp_hot[0,0], exp_cold_higher[0,0] ] )
     std = np.array( [ exp_hot[1,0], exp_cold_higher[1,0] ] )
 
+    
     xx = np.full((len(singlet_phases), len(phase_differences)), phase_differences).transpose()
     T_index = np.nonzero(temperatures == plot_temperature)[0][0]
     theory_distinguished = np.moveaxis(np.array( [[ arrays_hot_distinguished[:,T_index],], [arrays_cold_higher_distinguished[:,T_index], ]] ), 0, -1)
     theory = np.moveaxis(np.array( [ arrays_hot[:,:,T_index], arrays_cold_higher[:,:,T_index] ] ), 0, -1) if (singlet_phase_distinguished is not None) else theory_distinguished
     
-    return xx, theory_distinguished, experiment, std
+    return phase_differences, theory_distinguished, experiment, std
 
 
 
