12e3a10188843e6c446ae152f4c27e36d0b35b0f
makszachary <mz.walewski@student.uw.edu.pl>
2024-06-26 10:57:52 +0200

    parameter_uncertainties.py modified

--- molscat_data/parameter_uncertainties.py
+++ molscat_data/parameter_uncertainties.py
@@ -158,12 +158,12 @@ def main():
 
     if args.DPhi:
         phase_differences, theory_distinguished, experiment, std = get_p0_vs_DPhi(singlet_phases=singlet_phases, phase_differences=phase_differences, so_scaling=0.0, energy_tuple=energy_tuple, singlet_phase_distinguished=singlet_phase_distinguished, temperatures=temperatures, plot_temperature=plot_temperature, input_dir_name=input_dir_name)
-        theory_distinguished = np.squeeze(theory_distinguished)
-        print(phase_differences.shape)
-        phase_differences = np.full_like(theory_distinguished.transpose(), phase_differences).transpose()
+        theory_distinguished = np.squeeze(theory_distinguished).transpose()
         print(phase_differences, theory_distinguished, experiment, std)
         print(phase_differences.shape, theory_distinguished.shape, experiment.shape, std.shape)
-        popt, perr, chisq = fit_data(spin_exchange, phase_differences, theory_distinguished)
+        popt, perr, chisq = fit_data(spin_exchange, phase_differences, theory_distinguished[0])
+        print(popt, perr, chisq)
+        popt, perr, chisq = fit_data(spin_exchange, phase_differences, theory_distinguished[1])
         print(popt, perr, chisq)
 
 if __name__ == '__main__':
