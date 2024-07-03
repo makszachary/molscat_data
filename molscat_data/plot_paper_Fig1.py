0bd4b437af851bbd6a60f26a6c59cb1d4ef54d9d
Maks Zachary Walewski <maks.walewski@gmail.com>
2024-07-03 16:51:38 +0200

    plot_paper_Fig1.py: added saving something to txt

--- molscat_data/plot_paper_Fig1.py
+++ molscat_data/plot_paper_Fig1.py
@@ -77,7 +77,8 @@ def plotFig1(singlet_phases: float | np.ndarray[float], phase_differences: np.nd
     figs_axes[1].append(_ax_chisq)
 
     np.savetxt(data_path.with_stem(data_path.stem+'_DPhi'), _xx, fmt = '%.4f')
-    np.savetxt(data_path.with_stem(data_path.stem+'_theory'), _theory.reshape((*_theory.shape[:2], -1)), fmt = '%.4f')
+    print(*_theory.shape[:1])
+    np.savetxt(data_path.with_stem(data_path.stem+'_theory'), _theory.reshape((*_theory.shape[:1], -1)), fmt = '%.4f')
     np.savetxt(data_path.with_stem(data_path.stem+'_theory_distinguished'), _theory_distinguished, fmt = '%.4f')
 
     figs_axes[2].append(figs[2].add_subplot())
