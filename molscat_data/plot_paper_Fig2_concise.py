ad01e02f19858e2c384beab483f558853a4e8002
makszachary <mz.walewski@student.uw.edu.pl>
2024-10-21 13:45:26 +0200

    plot_paper_Fig2_concise.py: added printing out some statements

--- molscat_data/plot_paper_Fig2_concise.py
+++ molscat_data/plot_paper_Fig2_concise.py
@@ -418,7 +418,7 @@ def plotP0VsMassWithPartialWavesToFig(fig, singlet_phase: float, triplet_phase: 
     fig_bar.tick_params(axis = 'both')
     fig_bar.get_yaxis().labelpad = 4
     fig_bar.set_ylabel('$L$', rotation = 0, va = 'baseline', ha = 'left')
-    fig_bar.yaxis.set_label_coords(2.5, 0.5)
+    fig_bar.yaxis.set_label_coords(2.25, 0.5)
 
     return fig, fig_ax, reduced_masses, theory
 
