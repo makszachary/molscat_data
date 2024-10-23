bd8759a5b49b6eaac2aa5ae44fbaffca25e143ad
makszachary <mz.walewski@student.uw.edu.pl>
2024-10-23 14:02:49 +0200

    plot_paper_Fig3_modified.py added, trying to add decomposition into partial waves to Fig3c

--- molscat_data/plot_paper_Fig3_modified.py
+++ molscat_data/plot_paper_Fig3_modified.py
@@ -256,8 +256,8 @@ def plotColorMapAndSectionstoFigs(fig0, fig1, phase_step_cm: float, phase_step_s
     print(f'{arrays_cold_lower_distinguished[:,::2,0].shape =}')
     print(f'{probability_arrays[T_index,:,:].shape = }')
     if phase_difference_distinguished is not None and fmf_colormap:
-        theory = np.transpose([*arrays_cold_lower_distinguished[:,::2,0], 
-                               *probability_arrays[T_index,:,:]]
+        theory = np.array([*np.transpose(arrays_cold_lower_distinguished[:,::2,0]), 
+                               *np.transpose(probability_arrays[T_index,:,:])]
                   )
     else:
         theory = np.moveaxis(arrays_cold_lower_distinguished[:,::2,0], 1, -1)
