10ee3744dc6593bfd318d11be1e8ac027ff56b51
makszachary <mz.walewski@student.uw.edu.pl>
2024-10-28 16:41:51 +0100

    plot_paper_Fig2_modified.py: changed color map for partial waves

--- molscat_data/plot_paper_Fig2_modified.py
+++ molscat_data/plot_paper_Fig2_modified.py
@@ -400,7 +400,8 @@ def plotP0VsMassWithPartialWavesToFig(fig, singlet_phase: float, triplet_phase: 
     norm = matplotlib.colors.Normalize(vmin=0, vmax=l_max, clip = False)
     # theory_colors = [color_map(norm(L)) for L in range(l_max+1)]
     theory_formattings = [ #{'color': even_color, 'linewidth': 1.25},
-                          *[{'color': color_map(norm(L)), 'linewidth': 0.25} for L in range(0,l_max+1)]
+                        #   *[{'color': color_map(norm(L)), 'linewidth': 0.25} for L in range(0,l_max+1)]
+                        *[{'color': color_map(norm(L)), 'linewidth': 0.5} for L in range(0,l_max+1)]
                         #   {'color': odd_color, 'linewidth': 1.25}
                           ]
 
