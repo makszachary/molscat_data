0711b29f0446196691b7055e0f76be63310c4188
Maks Zachary Walewski <maks.walewski@gmail.com>
2024-11-05 22:42:02 +0100

    plot_paper_SupplementaryFig2.py

--- molscat_data/plot_paper_SupplementaryFig2.py
+++ molscat_data/plot_paper_SupplementaryFig2.py
@@ -41,6 +41,13 @@ plots_dir_path = scratch_path / 'python' / 'molscat_data' / 'plots'
 pmf_path = data_dir_path / 'pmf' / 'N_pdf_logic_params_EMM_500uK.txt'
 pmf_array = np.loadtxt(pmf_path)
 
+def exponential_formatter(x, ndp = 0):
+    if float(x) == 0:
+        return '0'
+    s = '{x:0.{ndp:d}e}'.format(x=x, ndp=ndp)
+    m, e = s.split('e')
+    return r'${m:s}\times 10^{{{e:d}}}$'.format(m=m, e=int(e))
+
 def plotSectionsWithPartialVsTtoFig(fig, phase_step_sections: float, phase_differences: float | np.ndarray[float], phase_difference_distinguished: float, so_scaling: float, energy_tuple: tuple[float, ...], temperatures: tuple[float, ...] = (5e-4,), plot_temperatures = [1e-4, 1e-3, 1e-2], input_dir_name: str = 'RbSr+_tcpld_80mK_0.01_step', transfer_input_dir_name: str = 'RbSr+_tcpld_80mK_0.01_step', hybrid = False, plot_p0 = False, fmf_colormap = False, plot_nan = False,):
     nenergies = len(energy_tuple)
     E_min = min(energy_tuple)
@@ -297,7 +304,7 @@ def plotMagneticFieldtoFig(fig, magnetic_phases: tuple[tuple[float, float], ...]
         props = dict(boxstyle='round', facecolor='none', edgecolor='none')
         # props = dict(facecolor='none')
         # ax.text(0.03, 0.10, f'$\\Delta\\Phi_\\mathrm{{fit}} = {(magnetic_phases[0][1]-magnetic_phases[0][0])%1:.2f}\\pi$', va = 'center', ha = 'left', transform = ax.transAxes, bbox = props)
-        ax.text(0.03, 0.10, f'$\\Delta\\Phi_\\mathrm{{fit}} = {plot_temperatures[index]:.2e}\\,\\mathrm{{K}}$', va = 'center', ha = 'left', transform = ax.transAxes, bbox = props)
+        ax.text(0.03, 0.10, f'$T = {exponential_formatter(plot_temperatures[index])}\\,\\mathrm{{K}}$', va = 'center', ha = 'left', transform = ax.transAxes, bbox = props)
         ylabel = f'$p_\\mathrm{{eff}}$' if not plot_p0 else f'$p_0$'
         ax.set_ylabel(ylabel)
     
