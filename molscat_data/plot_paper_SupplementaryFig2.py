e6e810f3465a2238f003e4dbf57f97f2da167f12
Maks Zachary Walewski <maks.walewski@gmail.com>
2024-11-05 18:31:12 +0100

    plot_paper_SupplementaryFig2.py

--- molscat_data/plot_paper_SupplementaryFig2.py
+++ molscat_data/plot_paper_SupplementaryFig2.py
@@ -197,10 +197,17 @@ def plotSectionsWithPartialVsTtoFig(fig, phase_step_sections: float, phase_diffe
     # print(f'{probability_arrays[T_index,:,:].shape = }')
     ### now we the have (T, singlet_phase, L) indices on axes for probability_arrays and expected shape (21, 98, 50)
     if phase_difference_distinguished is not None and fmf_colormap:
-        theory = np.transpose([*np.transpose(arrays_cold_lower_distinguished[:,T_indices,0]), 
-                               *np.transpose(probability_arrays[T_indices,:,:(plot_l_max+1)], (0,2,1))],
-                  (0,2,1)
-                  )
+        theory = np.array([
+            np.transpose([
+                *np.transpose(arrays_cold_lower_distinguished[:,index,0]),
+                *np.transpose(probability_arrays[index,:,:(plot_l_max+1)])
+            ])
+            for index in T_indices
+        ])
+        # theory = np.transpose([*np.transpose(arrays_cold_lower_distinguished[:,T_indices,0]), 
+        #                        *np.transpose(probability_arrays[T_indices,:,:(plot_l_max+1)], (0,2,1))],
+        #           (0,2,1)
+        #           )
     ### powinien być na końcu kształt (T, singlet_phase, plot_l_max+2), czyli (3, 98, 21), bo dla każdej temperatury i Phis mamy plot_l_max+1 fal parcjalnych i jedno sumaryczne prawdopobieństwo
     else:
         theory = np.moveaxis(arrays_cold_lower_distinguished[:,::2,0], 1, -1)
