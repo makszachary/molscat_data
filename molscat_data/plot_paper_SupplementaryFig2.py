b18cae97d8094b994f17e58e1c6f32b9f3495d08
Maks Zachary Walewski <maks.walewski@gmail.com>
2024-11-05 18:32:29 +0100

    plot_paper_SupplementaryFig2.py

--- molscat_data/plot_paper_SupplementaryFig2.py
+++ molscat_data/plot_paper_SupplementaryFig2.py
@@ -192,8 +192,10 @@ def plotSectionsWithPartialVsTtoFig(fig, phase_step_sections: float, phase_diffe
     # print(f'{arrays_cold_lower_distinguished =}')
     # print(f'{arrays_cold_lower_distinguished.shape =}')
 
-    print(f'{arrays_cold_lower_distinguished[:,::2,0].shape =}')
-    print(f'{arrays_cold_lower_distinguished[:,T_indices,0].shape = }')
+    # print(f'{arrays_cold_lower_distinguished[:,::2,0].shape =}')
+    # print(f'{arrays_cold_lower_distinguished[:,T_indices,0].shape = }')
+    print(f'{np.transpose(arrays_cold_lower_distinguished[:,0,0]).shape = }')
+    print(f'{np.transpose(probability_arrays[index,:,:(plot_l_max+1)]).shape =}')
     # print(f'{probability_arrays[T_index,:,:].shape = }')
     ### now we the have (T, singlet_phase, L) indices on axes for probability_arrays and expected shape (21, 98, 50)
     if phase_difference_distinguished is not None and fmf_colormap:
