24df092332b9cda7d7d9b5e5748021eaa1790b99
makszachary <mz.walewski@student.uw.edu.pl>
2024-11-19 14:19:02 +0100

    plot_paper_Fig1.py modified to fit SciAdv formatting

--- molscat_data/plot_paper_Fig1.py
+++ molscat_data/plot_paper_Fig1.py
@@ -424,7 +424,7 @@ def main():
         temperatures = np.array(args.temperatures)
 
     if args.font is not None:
-        font_path = Path(__file__).parent / 'mpl_style_sheets' / 'fonts' / (args.font+'.ttf')
+        font_path = Path(r'~/.fonts/'+args.font+'.ttf')
         if not font_path.is_file(): NameError(f'{font_path.name} not found in the {font_path.parent} directory')
         matplotlib.font_manager.fontManager.addfont(font_path)
         prop = matplotlib.font_manager.FontProperties(fname=font_path)
